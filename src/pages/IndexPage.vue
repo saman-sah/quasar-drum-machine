dcfc3dd836111f479b44510eb43d2a1388496786
SamanSah <sahraei.saman91@gmail.com>
2023-05-15 12:57:15 +0330

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-page>
+    <q-header elevated>
+      <q-card class="my-card col">
+        <q-card-section class="bg-purple text-white">
+          <div class="text-h6">Drum Machine</div>
+        </q-card-section>
+      </q-card> 
+    </q-header>
+    <div class="q-pa-md row flex">
+      <div class="col">
+        <q-card class="my-card flex justify-between q-pa-sm">
+          <q-toggle
+          v-model="power"
+          checked-icon="check"
+          color="purple"
+          label="Power"
+          unchecked-icon="clear"
+          />
+          <q-btn color="black" label="Stop" />
+          <q-btn style="background: #FF0080; color: white" label="No Sound" />
+        </q-card>
+      </div>
+    </div>
+
+    <div class="q-pa-md row flex">
+      <div class="col">
+        <q-card class="my-card flex justify-between q-pa-sm ">
+          <q-btn
+            class="q-ma-sm sound-btn"
+            :style="{'cursor':power ? 'pointer' : 'not-allowed'}"
+            v-for="sound in sounds"
+            :key="sound.name"
+            size="35px"
+            round
+            :color="power ? 'purple' : 'grey'"
+            :label="sound.name"
+            @click="playAudio(sound.url)"
+          />
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import sounds from '../sounds.js'
+export default defineComponent({
+  name: 'IndexPage',
+  data() {
+    return {
+      power: true,
+      sounds
+    }
+  },
+  methods: {
+    playAudio(url) {
+      var audio = new Audio(url);
+      audio.play();	
+    }
+  },
+})
+</script>
+<style>
+.nav-top {
+    justify-content: space-between;
+    display: flex;
+    max-height: 50px !important;
+}
+.sound-btn .block{
+  font-size: 16px
+}
+</style>
---
+++ src/sounds.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        name: '1',
+        url: 'http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/start.ogg'
+    },
+    {
+        name: '2',
+        url: 'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/arrow.mp3'
+    },
+    {
+        name: '3',
+        url: 'http://commondatastorage.googleapis.com/codeskulptor-assets/Collision8-Bit.ogg'
+    },
+    {
+        name: '4',
+        url: 'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_hit.wav'
+    },
+    {
+        name: '5',
+        url: 'https://rpg.hamsterrepublic.com/wiki-images/d/db/Crush8-Bit.ogg'
+    },
+    {
+        name: '6',
+        url: 'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_shoot.wav'
+    },
+    {
+        name: '7',
+        url: 'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3'
+    },
+    {
+        name: '8',
+        url: 'http://commondatastorage.googleapis.com/codeskulptor-demos/pyman_assets/eatpellet.ogg'
+    },
+    {
+        name: '9',
+        url: 'http://commondatastorage.googleapis.com/codeskulptor-demos/pyman_assets/eatedible.ogg'
+    },
+]
